81b0b18c2ee5afa227fa89bb35ea60dd52b340b3
JuanAlon <elbartoy@gmail.com>
2024-05-06 16:26:10 -0600

    This map for google, page.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Map from '../components/Map';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Enbuss</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default HomePage;
